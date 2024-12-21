36746b45e8ed5079935541e9e4e87a4658d2486e
chanhyuk05 <24sunrin106@sunrint.hs.kr>
2024-12-22 08:01:24 +0900

    dbsaou

--- src/pages/kick/index.tsx
+++ src/pages/kick/index.tsx
@@ -6,7 +6,7 @@ export default function Kick(){
     return(
         <div className={styles.container} >
             <div className={styles.suggest}>
-                
+                <h2>'산책 30분 즐기기'</h2>
             </div>
             <div className={styles.refresh}>
                 <h1>새로고침</h1>
